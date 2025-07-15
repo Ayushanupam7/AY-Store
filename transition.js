592178e12095ab0d081e8fafc4411b06721d5a3e
Your Name <you@example.com>
2025-07-15 13:20:42 +0530

    modified:   Component/books/books.css 	modified:   app-details.html 	modified:   styles.css 	modified:   transition.js

--- transition.js
+++ transition.js
@@ -1,12 +1,10 @@
 // transition.js
 
-// Add loader element to body
 const loader = document.createElement("div");
 loader.id = "pageLoader";
-loader.innerHTML = `<div class="spinner"></div>`;
+loader.innerHTML = `<div class="loader-ay">AY</div>`;
 document.body.appendChild(loader);
 
-// Initially hide loader after page load
 window.addEventListener("load", () => {
   loader.style.display = "none";
 });
@@ -26,11 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       link.addEventListener("click", function (e) {
         e.preventDefault();
-
-        // Show loader immediately
         loader.style.display = "flex";
-
-        // Wait 300ms to feel smoother
         setTimeout(() => {
           window.location.href = href;
         }, 300);
